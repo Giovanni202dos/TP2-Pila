d687d251714f52e5e44f47d13730fe4ea90b34cc
Giovanni202dos <102113285+Giovanni202dos@users.noreply.github.com>
2022-05-23 14:47:59 -0300

    Add files via upload

---
+++ pila.py
@@ -0,0 +1,49 @@
+class nodoPila():
+    info, sig = None, None
+
+
+class Pila():
+
+    def __init__(self):                         #self(significa "en este")            
+        self.__tamanio = 0                      #self.__cima(los dos guiones abajos(__) es para ponerlos en privado)
+        self.__cima = None
+
+    def tamanio(self):
+        return self.__tamanio
+    
+    def apilar(self, dato):
+        nodo = nodoPila()
+        nodo.info = dato
+        nodo.sig = self.__cima
+        self.__cima = nodo
+        self.__tamanio += 1
+    
+    def desapilar(self):
+        dato = self.__cima.info
+        self.__cima = self.__cima.sig
+        self.__tamanio -= 1
+        return dato
+
+    def cima(self):
+        return self.__cima.info
+    
+    def pila_vacia(self):
+        return self.__cima is None
+
+# from random import randint
+
+# pila1 = Pila()
+# print(pila1.pila_vacia())
+
+# for i in range(10):
+#     num = randint(0, 100)
+#     print('numero generado', num)
+#     pila1.apilar(num)
+
+# print(pila1.tamanio())
+
+# while(not pila1.pila_vacia()):
+#     dato = pila1.desapilar()
+#     print('elemento desapilado', dato)
+
+# print(pila1.pila_vacia())
